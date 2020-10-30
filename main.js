32e9bb637a4e599a530d2a348909ef65e7b00ced
Sebastian Kiebert <sebastian.k8@schule.koeln>
2020-10-30 21:26:50 +0100

    Drag and Drop Functions added

--- main.js
+++ main.js
@@ -2,6 +2,34 @@ var btnAddItem = document.getElementById("btnAddItem");
 var btnAddMarket = document.getElementById("btnAddMarket");
 var tbxItem = document.getElementById("tbxItem");
 var tbxMarket = document.getElementById("tbxMarket");
+var listMarket = document.getElementById("UlSupermarket");
+var listItem = document.getElementById("UlItems");
+
+
+
+window.onload = function () {
+
+    var itemArray = ["Apfel", "Birne", "Schoko"];
+
+    for (let i = 0; i < itemArray.length; i++) {
+        const element = itemArray[i];
+        var addItem = document.createElement('li');
+        addItem.draggable = true;
+        addItem.appendChild(document.createTextNode(element));
+        listItem.appendChild(addItem);
+    }
+
+    var marketArray = ["Rewe", "Edeka", "Aldi"];
+
+    for (let i = 0; i < marketArray.length; i++) {
+        const element = marketArray[i];
+        var addMarket = document.createElement('li');
+        addMarket.appendChild(document.createTextNode(element));
+        listMarket.appendChild(addMarket);
+    }
+
+};
+
 
 btnAddItem.onclick = function () {
     var item = tbxItem.value;
@@ -9,15 +37,16 @@ btnAddItem.onclick = function () {
         return;
     }
 
-    var list = document.getElementById("UlItems");
+    
 
     var addItem = document.createElement('li');
+    addItem.draggable = true;
     addItem.appendChild(document.createTextNode(item));
-    list.appendChild(addItem);
+    listItem.appendChild(addItem);
     
     tbxItem.value = null;
     tbxItem.focus();
-}
+};
 
 btnAddMarket.onclick = function () {
     var item = tbxMarket.value
@@ -25,14 +54,19 @@ btnAddMarket.onclick = function () {
         return;
     }
 
-    var list = document.getElementById("UlSupermarket");
+    
 
     var addItem = document.createElement('li');
+    addItem.draggable = false;
     addItem.appendChild(document.createTextNode(item));
-    list.appendChild(addItem);
+    listMarket.appendChild(addItem);
    
     tbxMarket.value = null;
     tbxMarket.focus();
-}
+};
 
-
+$(function () {
+    $("#UlItems, #UlSupermarket").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+});
