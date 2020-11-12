1689b7e60e0e11800ef494433bb9948150569142
Sebastian <Kiebert>
2020-11-12 14:44:50 +0100

    Market is bold now

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ window.onload = function () {
     for (let i = 0; i < itemArray.length; i++) {
         const element = itemArray[i];
         var addItem = document.createElement('li');
+        
         addItem.draggable = true;
         addItem.appendChild(document.createTextNode(element));
         listItem.appendChild(addItem);
@@ -24,6 +25,7 @@ window.onload = function () {
     for (let i = 0; i < marketArray.length; i++) {
         const element = marketArray[i];
         var addMarket = document.createElement('li');
+        addMarket.className = 'ulmarket';
         addMarket.appendChild(document.createTextNode(element));
         listMarket.appendChild(addMarket);
     }
@@ -32,41 +34,72 @@ window.onload = function () {
 
 
 btnAddItem.onclick = function () {
+    // Saves the value in a variable
     var item = tbxItem.value;
+
+    // Checks if it isnt empty
     if (item == "") {
+        // if it is empty it will do nothing and return
         return;
     }
-
     
-
+    // Creates list entry
     var addItem = document.createElement('li');
+
+    // Cant be moved around
     addItem.draggable = true;
+
+    // give item name from nameBox
     addItem.appendChild(document.createTextNode(item));
+
+    // Adds it to the list
     listItem.appendChild(addItem);
     
+    // Clears boxes
     tbxItem.value = null;
+
+    // Curser is in box
     tbxItem.focus();
 };
 
 btnAddMarket.onclick = function () {
+    // Saves the value in a variable
     var item = tbxMarket.value
+
+    // Checks if it isnt empty
     if ( item == "") {
+        // if it is empty it will do nothing and return
         return;
     }
 
-    
-
+    // Creates list entry
     var addItem = document.createElement('li');
+
+    // gives the item a class which will make the text bold
+    addMarket.className = 'ulmarket';
+
+    // Cant be moved around
     addItem.draggable = false;
+
+    // give item name from nameBox
     addItem.appendChild(document.createTextNode(item));
+
+    // Adds it to the list
     listMarket.appendChild(addItem);
    
+    // Clears boxes
     tbxMarket.value = null;
+
+    // Curser is in box
     tbxMarket.focus();
 };
+
 
 $(function () {
     $("#UlItems, #UlSupermarket").sortable({
         connectWith: ".connectedSortable"
     }).disableSelection();
 });
+
+
+
